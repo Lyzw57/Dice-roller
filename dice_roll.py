1b507008b83030f0b8ca520584ba2d2c55bbc6a4
Lyzw57 <askuczynski57@gmail.com>
2019-02-24 14:01:24 +0100

    created 'roll_the_dice' function in bonus version

--- dice_roll.py
+++ dice_roll.py
@@ -1,13 +1,34 @@
+# def roll_the_dice(dice: str):
+#     """
+#     non bonus version.
+#     """
+#     from random import randint
+#     amount, size = dice.split("d")
+#     result = 0
+
+#     for i in range(1, int(amount) + 1):
+#         result += randint(1, int(size))
+
+#     return result
+
 def roll_the_dice(dice: str):
+    """
+    non bonus version.
+    """
     from random import randint
     amount, size = dice.split("d")
-    result = 0
+    results = []
 
+    print
     for i in range(1, int(amount) + 1):
-        result += randint(1, int(size))
+        results.append(randint(1, int(size)))
 
+    result = str(sum(results)) + ":"
+    for each in results:
+        result += " "
+        result += str(each)
+        
     return result
-
 
 if __name__ == "__main__":
     for example in ('3d6', '4d12', '1d10', '5d4'):
